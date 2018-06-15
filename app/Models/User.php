03e6300e4968395f4f6e1a69cace18da6f6d61bd
ImVicChen <imvicchen@163.com>
2018-06-15 06:49:25 +0000

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,23 +9,18 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 
     public function topics()
     {
